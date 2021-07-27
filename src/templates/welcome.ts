e432bb82bc0ebc6b4b4ebd71fb9c304c32e76001
Kadievka Salcedo <ksalcedo@EMPCCS.LOCAL>
2021-07-27 18:36:10 -0400

    format

--- src/templates/welcome.ts
+++ src/templates/welcome.ts
@@ -6,6 +6,7 @@ const welcomeTemplate = `
 <html lang="en">
   <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
     <title>NestJs</title>
   </head>
   <body>
