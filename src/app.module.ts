734948f42fb64e49324595d85af325fb625fea88
Kadievka Salcedo <ksalcedo@EMPCCS.LOCAL>
2021-07-28 09:28:29 -0400

    Refactor to make index.html public

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 @Module({
@@ -13,7 +12,7 @@ import { UsersModule } from './users/users.module';
     MongooseModule.forRoot(process.env.DB_CLUSTER),
     UsersModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.API_DOCS, app, document);
-
+  app.use(express.static(__dirname + '/../public'));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
